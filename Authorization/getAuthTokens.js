3a7ae2868f4d4a1253ac2998f4082ded8918fb35
SteakFisher <88953813+SteakFisher@users.noreply.github.com>
2022-12-02 01:08:03 +0400

    Completed auth part
    Database management pending

---
+++ Authorization/getAuthTokens.js
@@ -0,0 +1,52 @@
+import fetch from 'node-fetch';
+
+import dotenv from 'dotenv';
+import Constants from '../Creds/Constants.json' assert {type: 'json'};
+
+dotenv.config()
+
+export default function getAuthTokens(state, app) {
+    return new Promise((resolve, reject) => {
+        app.get('/microsoft/auth', async function(request, response) {
+            try {
+
+                if (request.url.indexOf('/microsoft/auth') > -1) {
+                    const qs = new URL(request.url, Constants.authRedirect).searchParams;
+
+                    if (qs.get("state") === state) {
+                        const code = qs.get('code')
+                        response.end('Authentication successful! You can now close this window.');
+
+                        const params = new URLSearchParams();
+                        params.append("grant_type", "authorization_code");
+                        params.append("code", code);
+                        params.append("client_id", process.env.AZURE_CLIENT_ID);
+                        params.append("redirect_uri", Constants.authRedirect);
+                        params.append("client_secret", process.env.AZURE_CLIENT_SECRET);
+
+                        fetch('https://login.microsoftonline.com/common/oauth2/v2.0/token', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded',
+                                'scope': "offline_access user.read mail.read",
+                            },
+                            body: params
+                        })
+                            .then(res => res.json())
+                            .then(json => {
+                                resolve({
+                                    accessToken: json.access_token,
+                                    refreshToken: json.refresh_token,
+                                });
+                            })
+                    }
+                }
+            } catch (e) {
+                console.log("Then WHAT?")
+                console.log(e)
+            }
+        })
+        console.log(6)
+    })
+}
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 
 import getAuthLink from "./Authorization/getAuthLink.js";
+import getAuthTokens from "./Authorization/getAuthTokens.js";
 
 let app = express()
 
@@ -10,4 +11,14 @@ app.listen(process.env.PORT || 443, function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 })
 
+app.get('/', async function(request, response) {
+    response.send('hi');
+})
+
+// Pass in a unique state string to prevent CSRF attacks, each user is identified by the state u pass in
 getAuthLink("1234")
+
+
+// For each unique "state" passed in a new instance is spun up, forcing each user to have a unique state
+// This part MAYBE prone to breaking, so extensive testing is required
+console.log(await getAuthTokens("1234", app))
