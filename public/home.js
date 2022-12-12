e715a896195743d6c0152184364d148a7269e345
Gilchrist <67920312+Agentgilspy@users.noreply.github.com>
2022-12-12 23:50:11 +0400

    Homepage

--- public/home.js
+++ public/home.js
@@ -1,7 +1,8 @@
-console.log('hi')
-fetch('http://localhost:443/getLink?state=1234')
-    .then((res) =>
-        res.json()
-            .then(json => {
-                window.location.href = json.url
-            })) 
+// fetch the link as the button is loaded
+
+const login = async () => {
+    const state = Math.floor(Math.random() * 9999999)
+    sessionStorage.state = state
+    const { url } = await (await fetch(`/getLink?state=${state}`)).json()
+    location = url
+}
