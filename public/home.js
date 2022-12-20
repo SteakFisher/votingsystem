aa2bccb6337dde924b86fc4f0afa86a8e008804f
Gilchrist <67920312+Agentgilspy@users.noreply.github.com>
2022-12-21 00:23:59 +0400

    Used Bootstrap

--- public/home.js
+++ public/home.js
@@ -1,8 +1,27 @@
 // fetch the link as the button is loaded
 
+const hex = {
+    Jupiter: '#0000FF',
+    Mars: '#D60301',
+    Saturn: '#DE790A',
+    Neptune: '#228B22',
+}
+
+const house = (button) => {
+    const house = button.id
+    const previous = document.querySelector('.selected')
+    previous.classList.remove('selected')
+    button.classList.add('selected')
+    document.querySelector('#left-bg').style.backgroundColor = `${hex[house]}`
+    document.querySelector('#right-bg').style.background = `linear-gradient(45deg, black, ${hex[house]})`
+
+
+}
 const login = async () => {
     const state = Math.floor(Math.random() * 9999999)
     sessionStorage.state = state
-    const { url } = await (await fetch(`/getLink?state=${state}`)).json()
+    const { url } = await (
+        await fetch(`/getLink?state=${state}`)
+    ).json()
     location = url
 }
