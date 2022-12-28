d79babb5c55f237593425c61ece33aefabe74f66
Gilchrist <67920312+Agentgilspy@users.noreply.github.com>
2022-12-29 00:19:31 +0400

    house page done

---
+++ public/house.js
@@ -0,0 +1,46 @@
+const codes = {
+    200: 'alert-success',
+    401: 'alert-danger',
+    500: 'alert-warning'
+}
+const createAlertDiv = (msg, code) => {
+    const div = document.createElement('div')
+    div.classList.add('alert', `${codes[code]}`)
+    div.innerText = msg
+    return div
+}
+
+const upload = async (button) => {
+    const usernames = document.getElementById('Usernames')
+    const houselist = document.getElementById('Houselist')
+
+    let error = false
+    for (const element of [usernames, houselist]) {
+        if (!element.files[0]) {
+            element.classList.add('is-invalid')
+            error = true
+        }
+        else {
+            element.classList.remove('is-invalid')
+        }
+    }
+    if (error) {
+        alert('Please Upload the Excel file in both fields')
+    }
+    else {
+        const formdata = new FormData()
+        formdata.append('usernames', usernames.files[0], usernames.files[0].name)
+        formdata.append('houselist', houselist.files[0], houselist.files[0].name)
+        formdata.append('state', sessionStorage.state)
+        button.remove()
+        const res = await fetch('/admin/house', {
+            method: "POST",
+            body: formdata
+        })
+        const msg = await res.text()
+        const alertDiv = createAlertDiv(msg, res.status)
+        const stateDiv = document.getElementById('state')
+        stateDiv.appendChild(alertDiv)
+    }
+
+}
