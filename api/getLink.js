e249a1d93792d9e961658f3c13a45372ab804dc5
SteakFisher <88953813+SteakFisher@users.noreply.github.com>
2023-01-15 01:36:03 +0400

    vercel tests

--- api/getLink.js
+++ api/getLink.js
@@ -3,10 +3,8 @@ const { Router } = require('express')
 
 const { getAuthLink } = require('../Authorization/getAuthLink')
 
+export default function getLink(req, res) {
 
-const router = Router()
-
-router.get('/', (req, res) => {
     const state = req.query.state
     const redirect = req.query.redirect || 'vote'
     if (state) {
@@ -16,6 +14,6 @@ router.get('/', (req, res) => {
         states.push(state)
 
     } else res.status(400).send('No State Provided')
-})
 
-module.exports = router
+
+}
