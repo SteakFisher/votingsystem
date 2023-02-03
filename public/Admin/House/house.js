0c5f06d98816f438fd4c4280911d5d5ebcfa80a6
Gilchrist <67920312+Agentgilspy@users.noreply.github.com>
2023-02-04 01:20:11 +0400

    Code to parse xlsx client side

--- public/Admin/House/house.js
+++ public/Admin/House/house.js
@@ -1,3 +1,60 @@
+const getData = async (filePath) => {
+    const buffer = await filePath.arrayBuffer()
+    const file = XLSX.readFile(buffer)
+
+    let data = []
+
+    const sheets = file.SheetNames
+
+    for (let i = 0; i < sheets.length; i++) {
+        const temp = XLSX.utils.sheet_to_json(
+            file.Sheets[file.SheetNames[i]])
+        temp.forEach((res) => {
+            data.push(res)
+        })
+    }
+    return data
+}
+
+const structureData = (usernames, houses) => {
+    let final = []
+    let errors = []
+
+    for (let i = 0; i < usernames.length; i++) {
+        for (let j = 0; j < houses.length; j++) {
+            if (String(usernames[i]['Adm. No.']) === houses[j]['Adm. No.']) {
+                if (usernames[i]['User'] === undefined) {
+                    errors.push(usernames[i])
+                    continue
+                }
+
+                let data = {
+                    'Adm. No.': usernames[i]['Adm. No.'],
+                    'Name': usernames[i]['Name'],
+                    'Class': usernames[i]['Class'],
+                    'Section': usernames[i]['Section'],
+                    'User': usernames[i]['User'],
+                    'House': houses[j]['House']
+                }
+
+                if (!data['House']) {
+                    data['House'] = 'undefined'
+                }
+
+                for (const key in data) {
+                    if (data[key] === undefined) {
+                        errors.push(data)
+                        break;
+                    }
+                }
+
+                final.push(data)
+            }
+        }
+    }
+    return [final, errors]
+}
+
 const codes = {
     200: 'alert-success',
     401: 'alert-danger',
@@ -29,15 +86,18 @@ const upload = async (button) => {
     }
     else {
         const formdata = new FormData()
-        formdata.append('usernames', usernames.files[0], usernames.files[0].name.replace(' ', '-'))
-        formdata.append('houselist', houselist.files[0], houselist.files[0].name.replace(' ', '-'))
+        const usernamesarr = await getData(usernames.files[0])
+        const houselistarr = await getData(houselist.files[0])
+        const [students, errors] = await structureData(usernamesarr, houselistarr)
+
+        formdata.append('students', JSON.stringify(students))
         formdata.append('state', sessionStorage.state)
         button.remove()
         const res = await fetch('/admin/house', {
             method: "POST",
             body: formdata
         })
-        const { msg, errors } = await res.json()
+        const { msg } = await res.json()
         const alertDiv = createAlertDiv(msg, res.status)
         const stateDiv = document.getElementById('state')
         stateDiv.appendChild(alertDiv)
